5cb9c8ef22068eebe6d08d4fb92fcf7c75f7a9af
Javier Piqueras <javipiquerasm@gmail.com>
2022-12-30 18:01:51 +0100

    Mobile and animations

---
+++ app.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const burger = document.querySelector('.jp-burger');
+    const nav = document.querySelector('.jp-navigation');
+    const navLinks = document.querySelectorAll('.jp-navigation li')
+    
+    burger.addEventListener('click', ()=> {
+        //toggle nav
+        nav.classList.toggle('jp-navigation-mobile-active')
+        //animate links
+        navLinks.forEach((link, index)=>{
+            if( link.style.animation ) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.7}s`;
+            }
+        })
+        burger.classList.toggle('jp-burger-line-toggle')
+    })
+}
+
+navSlide();
